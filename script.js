58e9d481455827b1c9bf2a9a2086ecb120e7aa72
gomaplys <133662473+RyanCoimbraAlmeida@users.noreply.github.com>
2025-04-03 19:31:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessbilidade=document.getElementById('opcoes-acessibilidade');
+    
+    botaoDeAcessibilidade.addEventListener('click',function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessbilidade.classList.toggle('apresenta-lista');
+    
+    const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+    
+   botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado)
+
+})
+    
+    
+const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+const diminuiFonteBotao = document.getElementById('dminuir-fonte');
+    
+
+const alternaContraste = document.getElementById('alterna-contraste')
+    
+    
+    })
